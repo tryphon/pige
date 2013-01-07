f7445ad5b172ba6b64ff0a67fb5ae16692dbe649
Alban Peignier <alban@tryphon.eu>
2013-01-07 11:08:06 +0100

    Fixes tune_file by creating tmp/ directory if needed

--- spec/support/tune_file.rb
+++ spec/support/tune_file.rb
@@ -1,8 +1,9 @@
 def tune_file(duration = 300, format = "wav")
+  FileUtils.mkdir_p "tmp"
   File.expand_path("tmp/tune-#{duration}.#{format}").tap do |file|
     unless File.exists?(file)
       system "sox -n -r 44100 #{file} synth #{duration} sine 1000" or 
-        raise "Can't create #{target}"
+        raise "Can't create #{file}"
     end
   end
 end
