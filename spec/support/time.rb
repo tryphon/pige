b2bb63266b1d98a6c0373042836ee9293ce1afed
Alban Peignier <alban@tryphon.eu>
2013-01-07 10:16:06 +0100

    Uses Time.strptime only when available (ruby 1.9)

--- spec/support/time.rb
+++ spec/support/time.rb
@@ -1,4 +1,8 @@
 def time(string)
   # Time.parse changes day and month between 1.8 and 1.9
-  Time.strptime string, "%m/%d/%Y %H:%M %Z"
+  if Time.respond_to? :strptime
+    Time.strptime string, "%m/%d/%Y %H:%M %Z"
+  else
+    Time.parse string
+  end
 end
